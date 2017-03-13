8d18ec0c8dfceca27d99a6498e34679bb8c31415
sagarthatte <sagarthatte@gmail.com>
2017-03-13 20:30:26 +1100

    Update service.js
    Changing request type.

--- services.js
+++ services.js
@@ -8,7 +8,7 @@ weatherApp.service('cityService', function() {
 weatherApp.service('weatherService', ['$resource', function($resource) {
     this.getWeather = function(city, days, appID) {
         
-        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
+        var weatherAPI = $resource("https://api.openweathermap.org/data/2.5/forecast/daily", {callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
 
         return weatherAPI.get({q: city, cnt: days, APPID: appID});
     }
